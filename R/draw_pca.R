898ffdc19c588802edc3063cdd1b19d3a41ef15e
Matthew Teasdale <matt@teasdale.xyz>
2014-10-07 13:13:18 +0100

    update

--- R/draw_pca.R
+++ R/draw_pca.R
@@ -3,17 +3,22 @@
 #'R function draw a PCA from an evec dataframe
 #'
 #'@param evec_df input dataframe
+#'@param sample_id
 #'@keywords smartpca
 #'@export
 #'@import ggplot2
 #'@examples
 #'draw_pca(evec_df)
 
-draw_pca <- function(evec_df){
+draw_pca <- function(evec_df, sample_id){
+
+  sample <- dat[evec_df$Fam_ID == "KRP",]
+  others <- dat[evec_df$Fam_ID != "KRP",]
 
   # plot PCA with ggplot2
-  plt <- ggplot2::ggplot(evec_df, ggplot2::aes(PC1, PC2)) +
-    ggplot2::geom_text(ggplot2::aes(label=Fam_ID, colour = Fam_ID), size=4, vjust=0)
+  plt <- ggplot2::ggplot(others, ggplot2::aes(PC1, PC2)) +
+    ggplot2::geom_text(ggplot2::aes(label=Fam_ID, colour = Fam_ID), size=4, vjust=0) +
+    ggplot2::geom_point(data=sample,colour="blue",pch=15,size=5)
 
   print(plt)
 
