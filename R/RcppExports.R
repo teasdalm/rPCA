68d4663833f4406d887ddabf5db50739f4580663
Matthew Teasdale <matt@teasdale.xyz>
2015-02-09 11:40:01 +0000

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,7 +2,7 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 #' Draw PCA
 #'
-#'R function draw a PCA from an evec dataframe
+#'R function to make a ped file homzygous
 #'
 #'@param input_file input file ped
 #'@param ouput_file output file ped
