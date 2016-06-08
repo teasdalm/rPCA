d53566c813a9873b74e7c9d2eb0c2176d36213bd
Matthew Teasdale <matthew@teasdale.xyz>
2016-06-08 16:42:12 +0100

    Update draw_pca.R

--- R/draw_pca.R
+++ R/draw_pca.R
@@ -4,20 +4,22 @@
 #'
 #'@param x dataframe
 #'@param id sample id ["XXX"]
+#'@param a pc1
+#'@param b pc2
 #'@export
 #'@import ggplot2
 #'@import RColorBrewer
 #'@examples
 #'draw_pca(x, ids="sample1")
 
-draw_pca <- function(x, ids="XXX"){
+draw_pca <- function(x, ids="XXX", a=PC1, b=PC2){
 
   sample <- subset(x, pop %in% ids)
   others <- subset(x, !pop %in% ids)
   my_colours <- brewer.pal(n = 12, name = 'Paired')
 
   # plot PCA with ggplot2
-  plt <- ggplot(others, aes(PC1, PC2)) +
+  plt <- ggplot(others, aes(a, b)) +
     geom_point(aes(colour=pop, shape=pop), size=3.5) +
     scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
     scale_color_manual(values=rep(my_colours, 10)) +
