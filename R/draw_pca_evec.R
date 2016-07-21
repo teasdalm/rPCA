8c75ff20b9570891c7e5b0d974dd5178c2381324
Matthew Teasdale <matthew@teasdale.xyz>
2016-07-21 11:05:12 +0100

    merge scripts into 3

---
+++ R/draw_pca_evec.R
@@ -0,0 +1,45 @@
+#' Draw PCA from smartpca evec file
+#'
+#'R function draw a PCA from an evec dataframe
+#'
+#'@param x dataframe
+#'@param ids sample ids c("XXX")
+#'@param pc_a x-axis pc
+#'@param pc_b y-axis pc
+#'@param plot_names do we want to plot sample names [FALSE]
+#'@param size_of_name size of name to plot[3]
+#'@export
+#'@import ggplot2
+#'@import RColorBrewer
+#'@examples
+#'draw_pca(x, ids="sample1")
+
+draw_pca_evec <- function(x, ids="XXX", pc_a="PC1", pc_b="PC2", plot_names =FALSE, size_of_name = 3){
+
+  sample <- subset(x, pop %in% ids)
+  others <- subset(x, !pop %in% ids)
+  my_colours <- brewer.pal(n = 12, name = 'Paired')
+
+  # The base plot
+  plt <- ggplot(others, aes_string(pc_a, pc_b)) +
+    geom_point(aes(colour=pop, shape=pop), size=3.5) +
+    scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
+    scale_color_manual(values=rep(my_colours, 10))
+
+  # no names plot blue squares
+  if(plot_names == FALSE){
+    plt <- plt +
+      geom_point(data=sample,colour="blue",pch=15,size=5) +
+      guides(col = guide_legend(ncol = 3))
+
+    plt
+  # plot names
+  } else{
+    plt <- plt +
+      geom_text(data = sample, aes(label = id),size = size_of_name) +
+      guides(col = guide_legend(ncol = 3))
+    plt
+  }
+
+}
+
--- R/draw_pca_laser.R
+++ R/draw_pca_laser.R
@@ -3,17 +3,50 @@
 #'R function draw a PCA from a LASER dataframe
 #'
 #'@param x dataframe of reference data
+#'@param y dataframe of sample data
+#'@param pc_a x-axis pc
+#'@param pc_b y-axis pc
+#'@param plot_names do we want to plot sample names [FALSE]
+#'@param size_of_name size of name to plot[3]
 #'@export
 #'@import ggplot2
 #'@import RColorBrewer
 
-draw_pca_laser <- function (x)
-{
+draw_pca_laser <- function (x, y=NULL, pc_a="PC1", pc_b="PC2", bam_file_string = "-merge-rmdup", plot_names = FALSE, size_of_name = 3) {
   others <- x
   my_colours <- brewer.pal(n = 12, name = "Paired")
-  plt <- ggplot(others, aes(PC1, PC2)) + geom_point(aes(colour = popID,
-    shape = popID), size = 3.5) + scale_shape_manual(values = rep(c(1:10,
-    12:20), 10)) + scale_color_manual(values = rep(my_colours,
-    10))
-  plt
+
+  # do we have sample data
+  if(is.null(y)){
+    plt <- ggplot(others, aes_string(pc_a, pc_b)) +
+      geom_point(aes(colour = popID,shape = popID), size = 3.5) +
+      scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
+      scale_color_manual(values = rep(my_colours,10))
+    plt
+
+  } else {
+    sample <- y
+    sample$indivID <- gsub(bam_file_string, "", sample$indivID)
+    my_colours <- brewer.pal(n = 12, name = "Paired")
+
+    # base plot
+    plt <- ggplot(others, aes_string(pc_a, pc_b)) +
+      geom_point(aes(colour = popID,shape = popID), size = 3.5) +
+      scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
+      scale_color_manual(values = rep(my_colours,10))
+
+    # plot with names
+    if (plot_names == TRUE){
+      plt <- plt +
+        geom_text(data = sample, aes(label = indivID),size = size_of_name ) +
+        guides(col = guide_legend(ncol = 3))
+      plt
+    # plot blue squares
+    } else{
+      plt <- plt +
+        geom_point(data=sample,colour="blue",pch=15,size=5) +
+        guides(col = guide_legend(ncol = 3))
+      plt
+    }
+  }
 }
