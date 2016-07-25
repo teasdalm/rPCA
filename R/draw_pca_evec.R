b15ea57f6c1da0b12f65f8b6e6a8381355953c95
Matthew Teasdale <matthew@teasdale.xyz>
2016-07-25 15:26:16 +0100

    change pc labels

--- R/draw_pca_evec.R
+++ R/draw_pca_evec.R
@@ -4,8 +4,8 @@
 #'
 #'@param x dataframe
 #'@param ids sample ids
-#'@param pc_a x-axis pc
-#'@param pc_b y-axis pc
+#'@param pc_x x-axis pc
+#'@param pc_y y-axis pc
 #'@param plot_names do we want to plot sample names
 #'@param size_of_name size of name to plot
 #'@export
@@ -14,13 +14,13 @@
 #'@examples
 #'draw_pca_evec(x, ids=c("Sample1", "Sample2"), plot_names=TRUE)
 
-draw_pca_evec <- function(x, ids=NULL, pc_a="PC1", pc_b="PC2", plot_names =FALSE, size_of_name = 3){
+draw_pca_evec <- function(x, ids=NULL, pc_x="PC1", pc_y="PC2", plot_names =FALSE, size_of_name = 3){
 
   my_colours <- brewer.pal(n = 12, name = 'Paired')
 
   if(is.null(ids)){
     others <- x
-    plt <- ggplot(others, aes_string(pc_a, pc_b)) +
+    plt <- ggplot(others, aes_string(pc_x, pc_y)) +
       geom_point(aes(colour=pop, shape=pop), size=3.5) +
       scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
       scale_color_manual(values=rep(my_colours, 10))
@@ -31,7 +31,7 @@ draw_pca_evec <- function(x, ids=NULL, pc_a="PC1", pc_b="PC2", plot_names =FALSE
     others <- subset(x, !pop %in% ids)
 
     # The base plot
-    plt <- ggplot(others, aes_string(pc_a, pc_b)) +
+    plt <- ggplot(others, aes_string(pc_x, pc_y)) +
       geom_point(aes(colour=pop, shape=pop), size=3.5) +
       scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
       scale_color_manual(values=rep(my_colours, 10))
--- R/draw_pca_laser.R
+++ R/draw_pca_laser.R
@@ -4,8 +4,8 @@
 #'
 #'@param x dataframe of reference data
 #'@param y dataframe of sample data
-#'@param pc_a x-axis pc
-#'@param pc_b y-axis pc
+#'@param pc_x x-axis pc
+#'@param pc_y y-axis pc
 #'@param bam_file_string string to remove from sample names
 #'@param plot_names do we want to plot sample names
 #'@param size_of_name size of name to plot
@@ -13,13 +13,13 @@
 #'@import ggplot2
 #'@import RColorBrewer
 
-draw_pca_laser <- function (x, y=NULL, pc_a="PC1", pc_b="PC2", bam_file_string = NULL, plot_names = FALSE, size_of_name = 3) {
+draw_pca_laser <- function (x, y=NULL, pc_x="PC1", pc_y="PC2", bam_file_string = NULL, plot_names = FALSE, size_of_name = 3) {
   others <- x
   my_colours <- brewer.pal(n = 12, name = "Paired")
 
   # do we have sample data
   if(is.null(y)){
-    plt <- ggplot(others, aes_string(pc_a, pc_b)) +
+    plt <- ggplot(others, aes_string(pc_x, pc_y)) +
       geom_point(aes(colour = popID,shape = popID), size = 3.5) +
       scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
       scale_color_manual(values = rep(my_colours,10))
@@ -33,7 +33,7 @@ draw_pca_laser <- function (x, y=NULL, pc_a="PC1", pc_b="PC2", bam_file_string =
     my_colours <- brewer.pal(n = 12, name = "Paired")
 
     # base plot
-    plt <- ggplot(others, aes_string(pc_a, pc_b)) +
+    plt <- ggplot(others, aes_string(pc_x, pc_y)) +
       geom_point(aes(colour = popID,shape = popID), size = 3.5) +
       scale_shape_manual(values = rep(c(1:10,12:20), 10)) +
       scale_color_manual(values = rep(my_colours,10))
